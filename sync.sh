aea58c3ea06a7ec03b9318f7497b57f36c50272f
Ruoshi_Sun <ruoshi@gmail.com>
2024-01-26 12:51:39 -0500

    sync sync.sh

--- sync.sh
+++ sync.sh
@@ -2,24 +2,6 @@
 # Ruoshi Sun
 # 2024-01-24
 
-<<<<<<< HEAD
-if [ $# -eq 0 ]; then
-    echo "Usage: `basename $0` p|d [dry]"
-    echo "  p = production"
-    echo "  d = derp"
-    exit 1
-fi
-
-if [ $# -eq 2 ]; then
-    # dry run
-    echo "DRY RUN - no changes will be made in $HERE"
-    RSYNCFLAG=-na
-else
-    RSYNCFLAG=-qa
-fi
-
-=======
->>>>>>> 1a2537c (sync sync.sh)
 #-------------------------------
 # Global variables and functions
 #-------------------------------
@@ -46,45 +28,6 @@ function sync() {
         -exec rsync -qa {} $TMP \;
 }
 
-<<<<<<< HEAD
-#----------------------------
-# Initialize branch variables
-#----------------------------
-module purge
-case $1 in
-    p)
-        EBVER=4.7.1
-        APPSDIR="/sfs/applications/${YMp}_build"
-        PYTHON=/usr/bin/python3
-        BRANCH=main
-        git checkout main
-        ;;
-    d)
-        EBVER=4.7.1
-        APPSDIR="/sfs/applications/${YMd}_build"
-        PYTHON=/usr/bin/python3
-        git checkout derp
-        ;;
-    *)
-        echo "Invalid option"
-        exit 1
-        ;;
-esac
-
-EBDIR=$APPSDIR/software/EasyBuild/$EBVER
-PYVER=$($PYTHON -V|awk '{print $2}') # e.g. 3.6.8
-PYVER=${PYVER%.*}                    # e.g. 3.6
-# the trailing slash matters for rsync
-EASYBLOCKSDIR=$APPSDIR/software/EasyBuild/$EBVER/lib/python${PYVER}/site-packages/easybuild/easyblocks/
-SOFTWAREDIR=$APPSDIR/software/standard
-
-for i in $APPSDIR $EBDIR $EASYBLOCKSDIR $SOFTWAREDIR; do
-    if [ ! -d $i ]; then
-        echo "Error: $i is not a valid directory"
-        exit 1
-    fi
-done
-=======
 # check args
 if [ $# -eq 0 ]; then
     echo "Usage: `basename $0` p|d [dry]"
@@ -92,7 +35,6 @@ if [ $# -eq 0 ]; then
     echo "  d = derp"
     exit 1
 fi
->>>>>>> 1a2537c (sync sync.sh)
 
 if [ $# -eq 2 ]; then
     # dry run
@@ -171,13 +113,6 @@ rsync $RSYNCFLAG $EXCLUDE "$EASYBLOCKSDIR" $HERE/easyblocks
 echo "Done."
 echo $DIVIDER
 
-<<<<<<< HEAD
-echo "Pushing to GitHub... "
-git add -A
-DATE=$(date -Iseconds)
-git commit -m $DATE
-git push
-=======
 DATE=$(date -Iseconds)
 if [ $# -ne 2 ]; then
     echo "Pushing to GitHub... "
@@ -185,5 +120,4 @@ if [ $# -ne 2 ]; then
     git commit -m $DATE
     git push
 fi
->>>>>>> 1a2537c (sync sync.sh)
 echo "Completed on $DATE."
