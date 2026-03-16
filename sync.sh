1bde0997d7c0d31d6d2f550ddcaff7b04b9e194d
Ruoshi_Sun <ruoshi@gmail.com>
2026-03-16 12:00:41 -0400

    fix production eb version

--- sync.sh
+++ sync.sh
@@ -61,7 +61,7 @@ case $1 in
             echo "Error: trying to sync production into a branch other than main"
             exit 1
         fi
-        EBVER=5.2.0
+        EBVER=5.1.1
         APPSDIR="/sfs/weka/applications/${YMp}_build"
         PYTHON=/usr/bin/python3
         ;;
